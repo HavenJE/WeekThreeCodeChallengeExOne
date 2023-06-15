c1291d6c73a47d5ee4a8e1c843644d207a0a4b96
Haven <haven.eldebaque@gmail.com>
2023-06-16 00:18:36 +1000

    Add project files.

---
+++ WeekThreeCodeChallengeExOne/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+using WeekThreeCodeChallengeExOne;
+
+class TestSoccerPlayer
+{
+    public static void Main()
+    {
+        SoccerPlayer player1 = new SoccerPlayer();
+        player1.PlayerName = "Jeff";
+        player1.JerseyNumber = 1;
+        player1.GoalsScored = 4;
+        player1.Assists = 1;
+        Console.WriteLine("Player name is: {0}, Jersey namuber is: {1}, he scored {2} goals, and assisted {3}", player1.PlayerName, player1.JerseyNumber, player1.GoalsScored, player1.Assists);
+    }
+}
---
+++ WeekThreeCodeChallengeExOne/SoccerPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekThreeCodeChallengeExOne
+{
+    internal class SoccerPlayer
+    {
+        private string playerName;
+        private int jerseyNumber;
+        private int goalsScored;
+        private int assists;
+
+        // Properties
+        public string PlayerName
+        { get; set; }
+
+        public int JerseyNumber
+        { get; set; }
+
+        public int GoalsScored
+        { get; set; }
+
+        public int Assists
+        { get; set; }
+
+    }
+
+}
